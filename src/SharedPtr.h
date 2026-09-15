66de88bdd8eda463c7bde814ed41ed8d72b594ab
reschan <minhtrung.06.13@gmail.com>
2026-09-15 16:28:46 -0600

    intiial commit

--- src/SharedPtr.h
+++ src/SharedPtr.h
@@ -3,10 +3,10 @@
 
 class ControlBlockBase {
 public:
-    ControlBlockBase(); // TODO: implement the default constructor.
+    ControlBlockBase() {}  // TODO: implement the default constructor.
 
     // dtor is virtual, so that we can call derived class's dtor from a ptr to this base class.
-    virtual ~ControlBlockBase(); // TODO: implement the destructor.
+    virtual ~ControlBlockBase() {}  // TODO: implement the destructor.
 
     // pure virtual function; must be overriden by derived classes
     virtual void* managedAddress() = 0;
@@ -18,20 +18,136 @@ public:
     long increment()
     {
         // TODO: increment refcount by 1 and return result.
+        refcount += 1;
+        return refcount;
     }
 
     long decrement()
     {
         // TODO: decrement refcount by 1 and return result.
+        refcount -= 1;
+        return refcount;
     }
 
     long refCount() const
     {
         // TODO: just return the refcount.
+        return refcount;
     }
 
 private:
     // TODO: add field(s) which both control block types need to have
+    long refcount;
 };
+
+template <typename T>
+class ControlBlock : public ControlBlockBase {
+public:
+    ControlBlock(T* ptr) : mPtr{ptr} {}
+
+    ~ControlBlock() override {
+        if (mPtr != nullptr) {
+            delete mPtr;
+        }
+    }
+
+    void* managedAddress() override { return mPtr; }
+
+private:
+    T* mPtr;
+    long refcount;
+};
+
+
+template <typename T>
+class SharedPtr {
+public:
+    SharedPtr() : mPtr{nullptr}, mBlockPtr{nullptr} {}
+
+    template <typename U>
+    SharedPtr(U* ptr) : mPtr{static_cast<T*>(ptr)} {
+        if (mBlockPtr == nullptr) {
+            mBlockPtr = new ControlBlock(ptr);
+        }
+        mBlockPtr->increment();
+    }
+    template <typename U>
+    SharedPtr(SharedPtr<U>& other) : mPtr{other.get()} {
+        mBlockPtr = other.getBlock();
+        mBlockPtr->increment();
+    };  // copy ctor
+    template <typename U>
+    SharedPtr(SharedPtr<U>&& other) : mPtr{other.get()} {
+        mBlockPtr = other.getBlock();
+    }  // move ctor
+
+    ~SharedPtr() {
+        
+        if (mPtr != nullptr) {
+            mBlockPtr->decrement();
+            if (mBlockPtr->refCount() == 0) {
+                delete mPtr;
+            }
+        }
+    }
+
+    void swap(SharedPtr<T>& other) {
+        T* tmp = this->get();
+        ControlBlockBase* tmp2 = this->getBlock();
+        mPtr = other.get();
+        mBlockPtr = other.getBlock();
+        other.set(tmp);
+        other.setBlock(tmp2);
+    }
+    
+    void reset() {
+        mBlockPtr->decrement();
+        mPtr = nullptr;
+    }
+
+    void reset(T* newPtr) {
+        mBlockPtr->decrement();
+        if (mBlockPtr->refCount() == 0 && mPtr != nullptr) {
+            delete mPtr;
+            delete mBlockPtr;
+        }
+        mPtr = newPtr;
+
+        mBlockPtr = new ControlBlock(newPtr);
+    }
+
+    ControlBlockBase* getBlock() { return mBlockPtr; }
+    void setBlock(ControlBlockBase* ptr) { mBlockPtr = ptr; }
+
+
+    T& operator*() const { return *mPtr; }
+    T& operator->() const { return mPtr; }
+    T* get() const { return mPtr; }
+    void set(T* ptr) { mPtr = ptr; }
+
+    bool operator==(const SharedPtr<T>& other) const { return other.get() == mPtr; }
+    bool operator!=(const SharedPtr<T>& other) const { return !(other.get() == mPtr); }
+
+    operator bool() const { return mPtr == NULL; }
+
+    long useCount() { return mBlockPtr->refCount(); }
+
+
+
+private:
+    T* release() {
+        T* tmp = mPtr;
+        mPtr = nullptr;
+        return tmp;
+    }
+
+    T* mPtr;
+    ControlBlockBase* mBlockPtr = nullptr;
+};
+
+template <typename T, typename... Args>
+SharedPtr<T> makeShared(Args&&... args) {
+    return SharedPtr<T>(new T(std::forward<Args>(args)...));
+}
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,38 @@
 #include <cstdlib>
+#include <iostream>
 #include "SharedPtr.h"
+struct Base {
+    virtual ~Base() = default;
+};
 
-int main() {
-   return EXIT_SUCCESS;
+struct Derived : Base {};
+int main() { 
+    SharedPtr<int> ptr = makeShared<int>(5);
+    std::cout << *ptr << std::endl;
+    std::cout << ptr.get() << std::endl;
+    ptr.reset();
+    std::cout << (ptr.get() == NULL) << std::endl;
+
+    ptr.reset(new int(25));
+    std::cout << *ptr << std::endl;
+    std::cout << ptr.get() << std::endl;
+
+    SharedPtr<int> ptr2(new int(125));
+    std::cout << *ptr2 << std::endl;
+    std::cout << ptr2.get() << std::endl;
+
+    ptr.swap(ptr2);
+    std::cout << "swapped ptrs" << std::endl;
+    std::cout << *ptr << std::endl;
+    std::cout << ptr.get() << std::endl;
+    std::cout << *ptr2 << std::endl;
+    std::cout << ptr2.get() << std::endl;
+
+    SharedPtr<int> mkunique = makeShared<int>(5);
+    std::cout << *mkunique << std::endl;
+
+    std::cout << "casting" << std::endl;
+    SharedPtr<Derived> ptr5 = makeShared<Derived>();
+    SharedPtr<Base> ptr6 = std::move(ptr5);
+    return EXIT_SUCCESS;
 }
